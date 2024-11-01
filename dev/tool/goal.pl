7b4c72f6cb2bdbb63ea8cfa32f7844bcaa58f862
Lichtkind <lichtkind@cpan.org>
2024-11-01 10:36:56 +0100

    started goal checker tool

--- dev/tool/goal.pl
+++ dev/tool/goal.pl
@@ -7,41 +7,47 @@ use File::Find;
 use File::Spec;
 use FindBin qw($Bin);
 
+say Cwd::cwd();
+
 BEGIN { chdir $Bin }
-use CPutils qw/has_status list_subdir read_index extract_version/;
+
+# use CPutils qw/has_status list_subdir read_index extract_version/;
+#my $name = 'Kephra CP';
+
+chdir '../goal';
+say Cwd::cwd;
+
+# use App::Goto::Dir;
+use A;
+
+say 'goal stubs can be loaded';
+
+#my @stages = read_index('stage');
+#say " = $name stages";
+#for my $name (@stages){
+#    say "   - $name ";
+#}
+#say '';
+
+#say " = $name modules";
+#for my $mod_dir (list_subdir('module')){
+#    say "   - $mod_dir";
+#    for my $md (list_subdir(File::Spec->catdir('module', $mod_dir))){
+#        say "       - $md" if has_status($md);
+#    }
+#}
+#say '';
+
+#say " = $name features";
+#say "   - $_" for list_subdir('feature');
+#say '';
+
+#say " = $name functions";
+#say "   - $_" for list_subdir('function');
+#say '';
 
 
-my $name = 'Kephra CP';
-
-
-chdir '..';
-
-my @stages = read_index('stage');
-say " = $name stages";
-for my $name (@stages){
-    say "   - $name ";
-}
-say '';
-
-say " = $name modules";
-for my $mod_dir (list_subdir('module')){
-    say "   - $mod_dir";
-    for my $md (list_subdir(File::Spec->catdir('module', $mod_dir))){
-        say "       - $md" if has_status($md);
-    }
-}
-say '';
-
-say " = $name features";
-say "   - $_" for list_subdir('feature');
-say '';
-
-say " = $name functions";
-say "   - $_" for list_subdir('function');
-say '';
-
-
-say extract_version('tool/lib/CPutils.pm');
+#say extract_version('tool/lib/CPutils.pm');
 #say getcwd();
 # File::Spec->catdir($Bin, 
 #my @dir = list_subdir ('.');
