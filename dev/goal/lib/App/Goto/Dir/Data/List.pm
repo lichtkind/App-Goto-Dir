f0ff2c6a8912cc3a6b0e13059bb66e3bb5b26655
Lichtkind <lichtkind@cpan.org>
2024-11-18 01:10:41 +0100

    complete data list API

--- dev/goal/lib/App/Goto/Dir/Data/List.pm
+++ dev/goal/lib/App/Goto/Dir/Data/List.pm
@@ -4,26 +4,34 @@
 package App::Goto::Dir::Data::List;
 use v5.20;
 use warnings;
-use App::Goto::Dir::Data::ValueType::Relations;
 use App::Goto::Dir::Data::Entry;
+use App::Goto::Dir::Data::Filter;
 
-sub new {}# ~name ~decription, @.entry, @.filter --> .list
-sub state {}              #                      --> %state ()
+sub new {} # ~name ~decription, @.entry, @.filter --> .list
+sub state {}               #                      --> %state ()
 
-sub add_entry { }         # .entry -- +pos       --> ?
-sub remove_entry {}       #           +pos       --> .entry
-sub get_entry {}          #           +pos       --> .entry
+sub name             {}    #                      --> ~name
+sub description      {}    #                      --> ~description
+sub rename           {}    #                ~name --> ?~name
+sub redescribe       {}    #         ~description --> ?~description
 
-sub all_entries {}        #                      --> @.entry
-sub processed_entries {}  #                      --> @.entry # filtered and ordered
+sub all_entries      {}    #                      --> @.entry
+sub entry_count      {}    #                      --> +
+sub get_entry        {}    #                 +pos --> ?.entry
+sub add_entry        {}    #       .entry -- +pos --> ?.entry
+sub remove_entry     {}    #                 +pos --> ?.entry
 
-sub add_filter  {}        # .filter ~state       --> .filter
-sub remove_filter {}      # ~filter_name         --> .filter
-sub get_state  {}         # ~filter_name         --> ~state             # := - inactive | - x o
-sub set_state  {}         # ~filter_name, ~state --> ~state                  x eXclude
+sub get_order        {}    #                      --> ~order
+sub set_order        {}    # ~order               --> ?~order
+sub processed_entries{}    #                      --> @.entry           # filtered and ordered
+sub report           {}    #                      --> ~report
+
+sub all_filter       {}    #                      --> @.filter
+sub add_filter       {}    #  .filter,      ~mode --> ?.filter
+sub remove_filter    {}    #  ~filter_name        --> ?.filter
+sub get_filter_mode  {}    #  ~filter_name        --> ?~mode            # := - inactive
+sub set_filter_mode  {}    #  ~filter_name, ~mode --> ?~mode            #    x eXclude
                                                                         #    o pass (OK)
                                                                         #    m mark
-sub get_order        {}   #                      --> ~order
-sub set_order        {}   # ~order               --> ~order
 
 1;
