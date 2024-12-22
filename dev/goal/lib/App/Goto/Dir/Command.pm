6a0d501e0797bad1d161ad80c919e853ae807445
Lichtkind <lichtkind@cpan.org>
2024-12-22 20:27:32 +0100

    wrote command API

--- dev/goal/lib/App/Goto/Dir/Command.pm
+++ dev/goal/lib/App/Goto/Dir/Command.pm
@@ -7,4 +7,19 @@ use warnings;
 use App::Goto::Dir::Data;
 use App::Goto::Dir::Help;
 
+
+sub add_list    {}  #                        ~list_name, ~decription --> !
+sub remove_list {}  #                        ~list_name              --> !
+sub change_list {}  #                        ~list_name              --> !
+sub show_lists  {}  #                                                -->
+
+sub add_entry    {} #        ~dir -- ~name, ~description, ~list_name --> !
+sub delete_entry {} #        $ID                                     --> !  # $ID  := ~name|#pos -- ~list
+sub select_entry {} #        $ID                                     --> !
+sub copy_entry   {} #        $ID_source -- $ID_target                --> !
+sub move_entry   {} #        $ID_source -- $ID_target                --> !
+sub remove_entry {} #        #pos       -- ~list_name                --> !
+
+sub show_help    {} #
+
 1;
